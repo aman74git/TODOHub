64c56157f3e6123a40a78bb108067e055eb51c76
Aman Kumar <amank966180@gmail.com>
2023-03-28 01:14:23 +0530

    small changes

--- back-end/database/db.js
+++ back-end/database/db.js
@@ -4,13 +4,13 @@ const db = require('mongoose');
 db.set('strictQuery', true);
 
 //connect to db
+
+// const URI_local = 'mongodb://localhost:27017/todoDB';
+const URI_cloud = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.be8wbb1.mongodb.net/todoDB?retryWrites=true&w=majority`;
+
 const connectDB = async () => {
-  // await db.connect('mongodb://localhost:27017/todoDB', {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  // });
   await db.connect(
-    `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.be8wbb1.mongodb.net/todoDB?retryWrites=true&w=majority`,
+    URI_cloud, // change this to URI_local if you want to use local db
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
